23a9fa980d289852bece4da3276ccc4887b4b448
brice <b@bnema.dev>
2026-03-18 14:57:50 +0100

    test: add end-to-end offscreen browser smoke test

---
+++ integration/offscreen_browser_test.go
@@ -0,0 +1,64 @@
+//go:build cef_integration
+
+package integration
+
+import (
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/bnema/purego-cef/cef"
+)
+
+type testRenderHandler struct {
+	painted chan struct{}
+}
+
+func (h *testRenderHandler) GetViewRect() cef.Rect {
+	return cef.Rect{W: 800, H: 600}
+}
+
+func (h *testRenderHandler) OnPaint(event cef.PaintEvent) {
+	select {
+	case h.painted <- struct{}{}:
+	default:
+	}
+}
+
+func TestOffscreenBrowserPaints(t *testing.T) {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	if err := cef.Init("", cef.DefaultSettings()); err != nil {
+		t.Fatal(err)
+	}
+	defer cef.Shutdown()
+
+	handler := &testRenderHandler{painted: make(chan struct{}, 1)}
+	client := cef.NewClient().WithRenderHandler(handler)
+
+	browser, err := cef.CreateBrowser(cef.BrowserConfig{
+		URL:       "data:text/html,<html><body>purego-cef</body></html>",
+		Width:     800,
+		Height:    600,
+		FrameRate: 30,
+		Client:    client,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer browser.Close()
+
+	timeout := time.After(10 * time.Second)
+	for {
+		select {
+		case <-handler.painted:
+			return
+		case <-timeout:
+			t.Fatal("timed out waiting for first paint")
+		default:
+			cef.DoMessageLoopWork()
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
