c40bea0f901e437c6acfcc41b809ce8a4fef78d3
brice <b@bnema.dev>
2026-04-28 05:40:31 +0200

    fix: use script for go:generate cef headers fallback

---
+++ scripts/generate-cef.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -eu
+
+headers_dir="${CEF_HEADERS:-${HOME}/.local/share/cef/include}"
+
+go run ./cmd/cefgen \
+  --headers-dir "$headers_dir" \
+  --capi-dir internal/capi \
+  --port-in-dir internal/ports/in \
+  --port-out-dir internal/ports/out \
+  --public-dir cef
