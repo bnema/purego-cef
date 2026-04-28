72d821e43545deddb19d29857ccab9c05dadb476
brice <b@bnema.dev>
2026-04-28 05:35:49 +0200

    Fix CEF callback parsing and generate env expansion

--- generate.go
+++ generate.go
@@ -1,7 +1,7 @@
 // NOTE: The sh -c invocation with shell variable expansion requires
 // a POSIX shell (Linux/macOS). Windows users should set CEF_HEADERS
 // explicitly or run the generator manually.
-//go:generate sh -c "go run ./cmd/cefgen --headers-dir ${CEF_HEADERS:-$HOME/.local/share/cef/include} --capi-dir internal/capi --port-in-dir internal/ports/in --port-out-dir internal/ports/out --public-dir cef"
+//go:generate sh -c "go run ./cmd/cefgen --headers-dir $${CEF_HEADERS:-$$HOME/.local/share/cef/include} --capi-dir internal/capi --port-in-dir internal/ports/in --port-out-dir internal/ports/out --public-dir cef"
 //go:generate mockery
 
 package puregocef
